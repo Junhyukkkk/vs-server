8d259239044ccf59f89410174375483d7b1a9acf
junhyukkkk <cjunk0304@gmail.com>
2026-03-12 23:23:07 +0900

    init: project init

---
+++ src/main/java/com/ject/vs/VsServerApplication.java
@@ -0,0 +1,13 @@
+package com.ject.vs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ject/vs/VsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ject.vs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
