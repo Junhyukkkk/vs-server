00a1ff0f34839f9c1ad5576b3736f2a536dfffa0
tlarbals824 <simkm0824@naver.com>
2026-04-29 21:52:09 +0900

    refactor: 도메인 계층 구조 개선 - BaseAggregateEntity 도입
    - BaseEntity: id만 관리
    - BaseTimeEntity extends BaseEntity: 시간 추적 (더 근본적)
    - BaseAggregateEntity extends BaseTimeEntity: 도메인 이벤트 (더 구체적)
    - ChatMessage extends BaseAggregateEntity: registerEvent()로 이벤트 등록
    - @DomainEvents/@AfterDomainEventPublication을 BaseAggregateEntity에 집중
    - 불필요한 AggregateRoot 인터페이스, DomainEventEntityListener, DomainEventPublisher 제거

--- src/main/java/com/ject/vs/chat/domain/ChatMessage.java
+++ src/main/java/com/ject/vs/chat/domain/ChatMessage.java
@@ -1,25 +1,20 @@
 package com.ject.vs.chat.domain;
 
 import com.ject.vs.chat.domain.event.ChatMessageSentEvent;
-import com.ject.vs.common.domain.AggregateRoot;
-import com.ject.vs.common.domain.BaseTimeEntity;
-import com.ject.vs.common.infrastructure.DomainEventEntityListener;
-import jakarta.persistence.*;
+import com.ject.vs.common.domain.BaseAggregateEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Table(name = "chat_message")
-@EntityListeners(DomainEventEntityListener.class)
 @Getter
 @NoArgsConstructor(access = PROTECTED)
-public class ChatMessage extends BaseTimeEntity implements AggregateRoot {
+public class ChatMessage extends BaseAggregateEntity {
 
     @Column(nullable = false)
     private Long voteId;
@@ -30,29 +25,16 @@ public class ChatMessage extends BaseTimeEntity implements AggregateRoot {
     @Column(nullable = false)
     private String content;
 
-    @Transient
-    private List<Object> domainEvents = new ArrayList<>();
-
     public static ChatMessage of(Long voteId, Long senderId, String content) {
         ChatMessage chatMessage = new ChatMessage();
         chatMessage.voteId = voteId;
         chatMessage.senderId = senderId;
         chatMessage.content = content;
-        chatMessage.domainEvents.add(new ChatMessageSentEvent(chatMessage));
+        chatMessage.registerEvent(new ChatMessageSentEvent(chatMessage));
         return chatMessage;
     }
 
     public boolean isBlank() {
         return content.isBlank();
-    }
-
-    @Override
-    public List<Object> getDomainEvents() {
-        return Collections.unmodifiableList(domainEvents);
-    }
-
-    @Override
-    public void clearDomainEvents() {
-        domainEvents.clear();
     }
 }
--- src/main/java/com/ject/vs/common/domain/AggregateRoot.java
+++
@@ -1,8 +0,0 @@
-package com.ject.vs.common.domain;
-
-import java.util.List;
-
-public interface AggregateRoot {
-    List<Object> getDomainEvents();
-    void clearDomainEvents();
-}
