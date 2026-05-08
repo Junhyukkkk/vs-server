9045e83a7c8623c3c75f4a4dabc6b25f04f69880
junhyukkkk <cjunk0304@gmail.com>
2026-05-08 15:36:46 +0900

    feat: add anonymous id cookie resolver and web mvc config

---
+++ src/main/java/com/ject/vs/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.ject.vs.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AnonymousIdResolver anonymousIdResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(anonymousIdResolver);
+    }
+}
