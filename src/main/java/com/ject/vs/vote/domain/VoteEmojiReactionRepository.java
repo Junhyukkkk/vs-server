176f96912c06bcd6e1e7e707ba2dc7c43057bf58
tlarbals824 <simkm0824@naver.com>
2026-05-10 14:18:01 +0900

    VoteEmojiReaction의 이모지 통계 쿼리 리턴 타입을 EmoijCount DTO로 수정 및 관련 클래스 추가

--- src/main/java/com/ject/vs/vote/domain/VoteEmojiReactionRepository.java
+++ src/main/java/com/ject/vs/vote/domain/VoteEmojiReactionRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -13,8 +14,13 @@ public interface VoteEmojiReactionRepository extends JpaRepository<VoteEmojiReac
 
     Optional<VoteEmojiReaction> findByVoteIdAndAnonymousId(Long voteId, String anonymousId);
 
-    @Query("SELECT r.emoji, COUNT(r) FROM VoteEmojiReaction r WHERE r.voteId = :voteId GROUP BY r.emoji")
-    java.util.List<Object[]> countByEmojiForVote(@Param("voteId") Long voteId);
+    @Query("""
+                SELECT new com.ject.vs.vote.domain.EmoijCount(r.emoji, COUNT(r))
+                FROM VoteEmojiReaction r 
+                WHERE r.voteId = :voteId 
+                GROUP BY r.emoji
+            """)
+    List<EmoijCount> countByEmojiForVote(@Param("voteId") Long voteId);
 
     long countByVoteId(Long voteId);
 }
