506965339211f4d910e1aa857c0efac99c23bfdc
junhyukkkk <cjunk0304@gmail.com>
2026-05-16 12:14:31 +0900

    feat: add FirebaseConfig bean

---
+++ src/main/java/com/ject/vs/config/FirebaseConfig.java
@@ -0,0 +1,35 @@
+package com.ject.vs.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${firebase.service-account-path}")
+    private String serviceAccountPath;
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream(
+                ResourceUtils.getFile(serviceAccountPath));
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging(FirebaseApp app) {
+        return FirebaseMessaging.getInstance(app);
+    }
+}
